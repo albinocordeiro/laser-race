88259422469ffe62074230282b5c7c317dfc7ccd
Albino <acordeiro@nymi.com>
2022-10-18 00:11:40 -0400

    cargo workspace + trying bevy + introduce cli options

--- race-control/src/main.rs
+++ race-control/src/main.rs
@@ -1,3 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Cli option definition
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Path for the laser sensor signal calibration file: SensorCalibration.toml
+    /// File format example in the this repo
+    #[arg(short, long , value_name="CALIBRATION_FILE", default_value="SensorCalibration.toml")]
+    calibration_file: PathBuf,
+
+    /// Arduino device path (ex.: /dev/ttyACM0)
+    #[arg(short, long, value_name="ARDUINO_0_DEVICE_PATH", default_value="/dev/ttyACM0")]
+    ard0_path: PathBuf,
+
+    /// Debug level
+    #[arg(short, long , action = clap::ArgAction::Count)]
+    debug: u8,
+}
 
 fn main() {
     println!("
@@ -6,5 +27,13 @@ fn main() {
         ░▀░▀░▀░▀░▀▀▀░▀▀▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀░▀░▀▀▀░▀▀▀
     ");
 
+    let cli = Cli::parse();
+}
 
+
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert()
 }
